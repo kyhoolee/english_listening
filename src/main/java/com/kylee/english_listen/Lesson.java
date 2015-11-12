d3fccb9fce64a9e4cd303a03af7778fd7b2545d5
kyhoolee <kyhoolee@gmail.com>
2015-11-12 19:11:17 +0700

    data model

--- src/main/java/com/kylee/english_listen/Lesson.java
+++ src/main/java/com/kylee/english_listen/Lesson.java
@@ -1,5 +1,52 @@
 package com.kylee.english_listen;
 
 public class Lesson {
+	private String id;
+	
+	private String topicId;
+	
+	private String title;
+	private String image;
+	
+	private String audioURL;
+	private String htmlContent;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTopicId() {
+		return topicId;
+	}
+	public void setTopicId(String topicId) {
+		this.topicId = topicId;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public String getAudioURL() {
+		return audioURL;
+	}
+	public void setAudioURL(String audioURL) {
+		this.audioURL = audioURL;
+	}
+	public String getHtmlContent() {
+		return htmlContent;
+	}
+	public void setHtmlContent(String htmlContent) {
+		this.htmlContent = htmlContent;
+	}
+	
+	
 
 }
--- src/main/java/com/kylee/english_listen/Topic.java
+++ src/main/java/com/kylee/english_listen/Topic.java
@@ -1,5 +1,53 @@
 package com.kylee.english_listen;
 
 public class Topic {
+	private String id;
+	private String title;
+	private String image;
+	private String level;
+	
+	private String description;
 
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getLevel() {
+		return level;
+	}
+
+	public void setLevel(String level) {
+		this.level = level;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
+	
 }
